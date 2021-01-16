ba49acab752e0bc13867b569d9f3d03e3f8bf676
smeghammer <73583806+smeghammer@users.noreply.github.com>
2021-01-16 13:25:37 +0000

    Update crawl.sh
    fixed missing arg!

--- crawl.sh
+++ crawl.sh
@@ -5,9 +5,10 @@ do
 	# call fetcher.py with python alias.
 	# required args:
 	#
+	# -s startpos
 	# -d nnn.nnn.nnn.nnn
 	# 
-	# where nnn.nnn.nnn.nnn is the ip address of the mongoDB server
+	# where nnn.nnn.nnn.nnn is the ip address of the mongoDB server and startpos is teh location to staret the crawl
 	# optional args:
 	# 
 	# -p nnnnn
@@ -15,6 +16,6 @@ do
 	# 
 	# where nnnnn is mongoDB port and dbstring is the name of the mongoDB collection to use
 	
-	python3.6 fetcher.py -d 192.168.1.106
+	python3.6 fetcher.py -d 192.168.1.106 - 6
 done
 
