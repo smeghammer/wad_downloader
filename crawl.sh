b2d884a976cd4dfac304339f696a06348cee774c
smeghammer <73583806+smeghammer@users.noreply.github.com>
2021-01-16 13:18:31 +0000

    Update crawl.sh
    added comments

--- crawl.sh
+++ crawl.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
+# loop until the user presses q key
 while [ "$INPUT_STRING" != "q" ]:
 do
+	# call fetcher.py with python alias.
+	# required args:
+	#
+	# -d nnn.nnn.nnn.nnn
+	# 
+	# where nnn.nnn.nnn.nnn is the ip address of the mongoDB server
+	# optional args:
+	# 
+	# -p nnnnn
+	# -n dbstring
+	# 
+	# where nnnnn is mongoDB port and dbstring is the name of the mongoDB collection to use
+	
 	python3.6 fetcher.py -d 192.168.1.106
 done
 
