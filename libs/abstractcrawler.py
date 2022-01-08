faff49c08a88abafeaf870b52cb5cf38746dab10
Silas <silas@thecoven.co.uk>
2022-01-08 14:24:28 +0000

    added base class methods __init__() and storeDownloadLink()

--- libs/abstractcrawler.py
+++ libs/abstractcrawler.py
@@ -5,24 +5,56 @@ Created on 10 Jan 2021
 '''
 
 from abc import ABC, abstractmethod
+import requests
+from bs4 import BeautifulSoup
 
 class AbstractCrawler(ABC):
     
-    @abstractmethod
-    def findSpiderLinks(self):
-        return
+    # @abstractmethod
+    # def findSpiderLinks(self):
+    #     return
+    #
+    # @abstractmethod
+    # def findDownloadLinks(self):
+    #     return
+    '''  initialise class instance with the url, root of API, root for downloads and a database 
+    object - we only want to instantiate the database object one time, so pass it around after
+    instantiating in the start module:  '''
+    @classmethod
+    def __init__(self,startnode,crawlroot,downloadRoot,database,crawlerId,counter=1):
+        self.url = crawlroot + startnode
+        self.downloadRoot = downloadRoot
+        self.crawlroot = crawlroot
+        self.db = database
+        self.crawlerId = crawlerId
+        self.counter=counter
+        self.soup = None
     
-    @abstractmethod
-    def findDownloadLinks(self):
-        return
+    # @abstractmethod
+    # def storeDownloadLink(self,linkData=None,url=None):
+    #     return
     
-    @abstractmethod
-    def storeDownloadLink(self,linkData=None):
-        return
+    '''  push the download link to the database, assembling as needed and flag as not fetched  '''
+    @classmethod
+    def storeDownloadLink(self,linkData,url):                                                    #or PENDNG or FETCHED
+        obj = {  
+            'url' : url, 
+            'state' : 'NOTFETCHED', 
+            'source':self.crawlerId,    
+            'metadata':linkData  
+        }
+        print(obj)
+        self.db.storeDownloadLinkObj(obj)
     
     @abstractmethod
     def open(self):
         return
+    
+    # @classmethod
+    # def open(self):
+    #     response = requests.get(self.url)
+    #     self.data = BeautifulSoup(response.content,'html.parser')
+    #     self.crawl(self)
     
     @abstractmethod
     def crawl(self):
