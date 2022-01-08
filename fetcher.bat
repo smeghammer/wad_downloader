d2c78ec34606ca150c315658ebe815ad4134ddee
Silas <silas@thecoven.co.uk>
2022-01-08 16:28:31 +0000

    added mesaaging and variable arg to fetcher.bat

--- fetcher.bat
+++ fetcher.bat
@@ -1,3 +1,4 @@
+@echo off
 rem ARGUMENTS:
 rem -d	the IP address of your mongo database server
 rem optionally, you can use
@@ -6,11 +7,26 @@ rem -n dbname for an alternate database name
 rem make sure to use the actual name of your python3 alias!
 
 
+set db=%1
+
+if "%db%"=="" (goto end)
+
 echo "running fetcher"
 
 :loop
 
-	python fetcher.py -d 192.168.1.123
+	python fetcher.py -d %db% 
 
 goto loop
+
+:end
+
+echo -------------------------------------------------
+echo - Note: You need an available MongoDB database! -
+echo - Please give database IP address:              -
+echo -                                               -
+echo - fetcher.bat [ip of mongoDB]                   -
+echo -                                               -
+echo -------------------------------------------------
+
 
